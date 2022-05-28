ddb4d82e3642cf07a2f3dad8b6d9419e97f89df5
Amer <amersakhtar@gmail.com>
2022-05-28 03:24:53 -0400

    Updated logger syntax

--- src/server/middleware/logger.js
+++ src/server/middleware/logger.js
@@ -1,4 +1,4 @@
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 
 const logger = (req, res, next) =>  {
     console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${dayjs().format()}`);
