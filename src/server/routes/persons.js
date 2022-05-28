272435bb4e62ec1abd1dce3c18490d5832bc2a53
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-28 15:09:15 -0400

    Removed unused routes

--- src/server/routes/persons.js
+++ src/server/routes/persons.js
@@ -5,19 +5,12 @@ import person from '../models/schemas/Person.js';
 const router = express.Router();
 
 router.get(process.env.REACT_APP_GETALLPERSONS_API, async (req, res, next) => { 
-    const persons = await person.findAllPersons.clone().catch(function(err) { console.log(err) });
-    res.send(persons);
+  const persons = await person.findAllPersons.clone().catch(function(err) { console.log(err) });
+  res.send(persons);
 });
-  
-// router.get(process.env.REACT_APP_POSTPERSON_API, async (req, res, next) => { 
-//     // console.log(req);
-//     // console.log(res);
-//     // res.send(req);
-//     // res.send(res);
-// });
-  
-router.post(process.env.REACT_APP_POSTPERSON_API, async (req, res, next) => { 
-  res.send('ebloooooga');
+
+router.post(process.env.REACT_APP_POSTPERSON_API, async (req, res, next) => {
+  res.send(process.env.RESPONSE_OK);
 });
 
 export default router;
