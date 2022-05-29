8e31ac6be4f144d81ffffecd4443007d17e2a784
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-29 04:52:11 -0400

    Renamed references to schemas

--- src/server/routes/person.js
+++ src/server/routes/person.js
@@ -5,13 +5,13 @@ import person from '../models/schemas/Person.js';
 const router = express.Router();
 
 router.get(process.env.REACT_APP_PERSONLIST_API, async (req, res, next) => { 
-  const persons = await person.findAllPersons.clone().catch(function(err) { console.log(err) });
+  const persons = await person.list.clone().catch(function(err) { console.log(err) });
   res.send(persons);
 });
 
 router.post(process.env.REACT_APP_PERSONCREATE_API, async (req, res, next) => {
   res.send(process.env.RESPONSE_OK);
-  console.log(`Request payload: ${JSON.stringify(req.body)}.`);
+  await person.create(req.body);
 });
 
 export default router;
