c6cafc8325d3c1d390570e59d17079d68ed4150c
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-29 00:29:59 -0400

    Added API configuration

--- src/api/getAllPersons.js
+++ src/api/getAllPersons.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL + process.env.REACT_APP_GETALLPERSONS_API
+  baseURL: process.env.REACT_APP_BASE_URL + process.env.REACT_APP_GETALLPERSONS_API,
+  timeout: 5000
 });
--- src/api/postPerson.js
+++ src/api/postPerson.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL + process.env.REACT_APP_POSTPERSON_API
+  baseURL: process.env.REACT_APP_BASE_URL + process.env.REACT_APP_POSTPERSON_API,
+  // headers: {"Content-Type": "application/json"},
+  // timeout: 10000
 });
