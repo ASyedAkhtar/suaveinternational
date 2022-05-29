5ec8e2d368a428d24950a1f8f8cb894e888deff3
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-29 05:05:40 -0400

    Updated formatting

--- src/components/js/apply.js
+++ src/components/js/apply.js
@@ -1,21 +1,22 @@
-class Person    {
-    constructor(minecraftName, discordName, socialType, socialName, ageGroup, info){
-        this.minecraftName = minecraftName;
-        this.discordName = discordName;
-        this.socialType = socialType;
-        this.socialName = socialName;
-        this.ageGroup = ageGroup;
-        this.info = info;
-    }
+class Person {
+  constructor(minecraftName, discordName, socialType, socialName, ageGroup, info) {
+    this.minecraftName = minecraftName;
+    this.discordName = discordName;
+    this.socialType = socialType;
+    this.socialName = socialName;
+    this.ageGroup = ageGroup;
+    this.info = info;
+  }
 }
 
-function onSubmit(e)    {
-    const applicant = new Person(document.querySelector('#minecraftName').value,
-            document.querySelector('#discordName').value,
-            document.querySelector(document.getElementById("reddit").checked ? '#reddit' : '#twitter').value,
-            document.querySelector('#socialName').value,
-            document.querySelector('#ageGroup option:checked').value,
-            document.querySelector('#information').value);
+function onSubmit(e) {
+  const applicant = new Person(document.querySelector('#minecraftName').value,
+    document.querySelector('#discordName').value,
+    document.querySelector(document.getElementById("reddit").checked ? '#reddit' : '#twitter').value,
+    document.querySelector('#socialName').value,
+    document.querySelector('#ageGroup option:checked').value,
+    document.querySelector('#information').value
+  );
     // e.preventDefault();
 }
 
