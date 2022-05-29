ab3c33ff455f03e5c2537a1566611a5e36e4a77d
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-29 05:03:30 -0400

    Rearranged comments

--- src/index.js
+++ src/index.js
@@ -16,6 +16,11 @@ createRoot(document.getElementById('root')).render(
   // </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
@@ -23,8 +28,3 @@ createRoot(document.getElementById('root')).render(
 //     ,
 //   document.getElementById('root')
 // );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
