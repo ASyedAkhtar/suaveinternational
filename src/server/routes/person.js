49bb207d448f42696b09f29c62dd1f2847b8a184
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-29 16:05:08 -0400

    Simplified list dereference

--- src/server/routes/person.js
+++ src/server/routes/person.js
@@ -5,7 +5,7 @@ import person from '../models/schemas/Person.js';
 const router = express.Router();
 
 router.get(process.env.REACT_APP_PERSONLIST_API, async (req, res, next) => { 
-  const persons = await person.list.clone().catch(function(err) { console.log(err) });
+  const persons = await person.list.clone();
   res.send(persons);
 });
 
