63785dc330aa417f8c79fb64b23e6e5804c9a834
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-29 19:40:56 -0400

    Renamed environment variables

--- src/api/person/create.js
+++ src/api/person/create.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL + process.env.REACT_APP_PERSON_ROUTE + process.env.REACT_APP_PERSONCREATE_API,
+  baseURL: process.env.REACT_APP_HOST_URL + process.env.REACT_APP_PERSON_ROUTE + process.env.REACT_APP_PERSON_CREATE_API,
   // headers: {"Content-Type": "application/json"},
   timeout: 5000
 });
