fd2a7fe53dbd25b4e805882e37b5933238571cf0
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-29 01:28:08 -0400

    Updated backend API structure to mirror front end

--- src/server/routes/person.js
+++ src/server/routes/person.js
@@ -4,14 +4,14 @@ import person from '../models/schemas/Person.js';
 
 const router = express.Router();
 
-router.get(process.env.REACT_APP_GETALLPERSONS_API, async (req, res, next) => { 
+router.get(process.env.REACT_APP_PERSONLIST_API, async (req, res, next) => { 
   const persons = await person.findAllPersons.clone().catch(function(err) { console.log(err) });
   res.send(persons);
 });
 
-router.post(process.env.REACT_APP_POSTPERSON_API, async (req, res, next) => {
+router.post(process.env.REACT_APP_PERSONCREATE_API, async (req, res, next) => {
   res.send(process.env.RESPONSE_OK);
-  console.log(`Request payload: ${JSON.stringify(req.body)}`);
+  console.log(`Request payload: ${JSON.stringify(req.body)}.`);
 });
 
 export default router;
