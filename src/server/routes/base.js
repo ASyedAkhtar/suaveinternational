7db9ed716a3710057497bb12aa13f5362b71bf4c
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-29 19:39:52 -0400

    Added server side redirect route

---
+++ src/server/routes/base.js
@@ -0,0 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import express from 'express';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const buildPath = path.join(__dirname, '../../..', 'build');
+
+const router = express.Router();
+
+router.get(process.env.REACT_APP_BASE_MISSING_API, async (req, res, next) => {
+    res.redirect(process.env.REACT_APP_BASE_ROUTE);
+});
+
+export default router;
