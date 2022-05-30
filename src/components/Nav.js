6c1d4704dc1c684f5e72952e54c90518ad8f16ed
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-30 16:23:03 -0400

    Added support for navigation icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,23 +6,23 @@ const Nav = () => {
     <div>
       <nav id='navigation' className='floatbar popout'>
         <div>
-          <ul>
-            <li><Link to={process.env.REACT_APP_BASE_URL}>Home</Link></li>
-            <li><Link to={process.env.REACT_APP_GALLERY_URL}>Gallery</Link></li>
-            <li><Link to={process.env.REACT_APP_APPLICATIONSTATUS_URL}>Application Status</Link></li>
-            <li><Link to={process.env.REACT_APP_HOWTOAPPLY_URL}>How To Apply</Link></li>
-            <li><Link to={process.env.REACT_APP_ABOUTUS_URL}>About Us</Link></li>
+          <ul className='navlinks'>
+            <li><Link to={process.env.REACT_APP_BASE_URL}></Link></li>
+            <li><Link to={process.env.REACT_APP_GALLERY_URL}></Link></li>
+            <li><Link to={process.env.REACT_APP_APPLICATIONSTATUS_URL}></Link></li>
+            <li><Link to={process.env.REACT_APP_HOWTOAPPLY_URL}></Link></li>
+            <li><Link to={process.env.REACT_APP_ABOUTUS_URL}></Link></li>
           </ul>
         </div>
       </nav>
       <nav id='social' className='floatbar popout'>
         <div>
-          <ul>
-            <li><a href={`https://www.youtube.com/channel${process.env.REACT_APP_YOUTUBE_EXTERNAL_URL}`} target='_blank' rel='noreferrer'>YouTube</a></li>
-            <li><a href={`https://www.instagram.com${process.env.REACT_APP_INSTAGRAM_EXTERNAL_URL}`} target='_blank' rel='noreferrer'>Instagram</a></li>
-            <li><a href={`https://www.twitter.com${process.env.REACT_APP_TWITTER_EXTERNAL_URL}`} target='_blank' rel='noreferrer'>Twitter</a></li>
-            <li><a href={`https://www.tiktok.com${process.env.REACT_APP_TIKTOK_EXTERNAL_URL}`} target='_blank' rel='noreferrer'>TikTok</a></li>
-            <li><a href={`https://www.facebook.com${process.env.REACT_APP_FACEBOOK_EXTERNAL_URL}`} target='_blank' rel='noreferrer'>Facebook</a></li>
+          <ul className='sociallinks'>
+            <li><a href={`https://www.youtube.com/channel${process.env.REACT_APP_YOUTUBE_EXTERNAL_URL}`} target='_blank' rel='noreferrer'></a></li>
+            <li><a href={`https://www.instagram.com${process.env.REACT_APP_INSTAGRAM_EXTERNAL_URL}`} target='_blank' rel='noreferrer'></a></li>
+            <li><a href={`https://www.twitter.com${process.env.REACT_APP_TWITTER_EXTERNAL_URL}`} target='_blank' rel='noreferrer'></a></li>
+            <li><a href={`https://www.tiktok.com${process.env.REACT_APP_TIKTOK_EXTERNAL_URL}`} target='_blank' rel='noreferrer'></a></li>
+            <li><a href={`https://www.facebook.com${process.env.REACT_APP_FACEBOOK_EXTERNAL_URL}`} target='_blank' rel='noreferrer'></a></li>
           </ul>
         </div>
       </nav>
