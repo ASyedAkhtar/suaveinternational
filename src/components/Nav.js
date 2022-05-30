cbfb7a73f60cae202c96a1ee4ca60c20b4530d39
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-30 04:36:55 -0400

    Removed Rules link and one external anchor link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,19 +4,18 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   return(
     <div>
-      <nav id='navigation' className='floatbar box popout'>
+      <nav id='navigation' className='floatbar popout'>
         <div>
           <ul>
             <li><Link to={process.env.REACT_APP_BASE_URL}>Home</Link></li>
             <li><Link to={process.env.REACT_APP_GALLERY_URL}>Gallery</Link></li>
-            <li><Link to={process.env.REACT_APP_RULES_URL}>Rules</Link></li>
             <li><Link to={process.env.REACT_APP_APPLICATIONSTATUS_URL}>Application Status</Link></li>
             <li><Link to={process.env.REACT_APP_HOWTOAPPLY_URL}>How To Apply</Link></li>
             <li><Link to={process.env.REACT_APP_ABOUTUS_URL}>About Us</Link></li>
           </ul>
         </div>
       </nav>
-      <nav id='social' className='floatbar box popout'>
+      <nav id='social' className='floatbar popout'>
         <div>
           <ul>
             <li><a href={`https://www.youtube.com/channel${process.env.REACT_APP_YOUTUBE_EXTERNAL_URL}`} target='_blank' rel='noreferrer'>YouTube</a></li>
@@ -24,7 +23,6 @@ const Nav = () => {
             <li><a href={`https://www.twitter.com${process.env.REACT_APP_TWITTER_EXTERNAL_URL}`} target='_blank' rel='noreferrer'>Twitter</a></li>
             <li><a href={`https://www.tiktok.com${process.env.REACT_APP_TIKTOK_EXTERNAL_URL}`} target='_blank' rel='noreferrer'>TikTok</a></li>
             <li><a href={`https://www.facebook.com${process.env.REACT_APP_FACEBOOK_EXTERNAL_URL}`} target='_blank' rel='noreferrer'>Facebook</a></li>
-            <li><a href={`https://www.snapchat.com/add${process.env.REACT_APP_SNAPCHAT_EXTERNAL_URL}`} target='_blank' rel='noreferrer'>Snapchat</a></li>
           </ul>
         </div>
       </nav>
