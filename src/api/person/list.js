9a63414c4e3b7be7d23ff09a102719a3688c0a98
ASyedAkhtar <amersakhtar@gmail.com>
2022-05-31 15:14:32 -0400

    Updated API calls to reference protocol

--- src/api/person/list.js
+++ src/api/person/list.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_HOST_URL + process.env.REACT_APP_PERSON_ROUTE + process.env.REACT_APP_PERSON_LIST_API,
+  baseURL: process.env.REACT_APP_HOST_PROTOCOL + process.env.REACT_APP_HOST_NAME + process.env.REACT_APP_PERSON_ROUTE + process.env.REACT_APP_PERSON_LIST_API,
   timeout: 5000
 });
