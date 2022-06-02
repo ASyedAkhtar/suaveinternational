7da084cc6df404ad2f575272d563fb510666a078
ASyedAkhtar <amersakhtar@gmail.com>
2022-06-01 22:49:09 -0400

    Added a route for GET request using '.'

--- src/server/routes/base.js
+++ src/server/routes/base.js
@@ -6,10 +6,17 @@ import express from 'express';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const buildPath = path.join(__dirname, '../../..', 'build');
 
+const hostNameHTTPS = process.env.REACT_APP_HOST_NAME_HTTPS;
+const hostName = hostNameHTTPS.substring(0, hostNameHTTPS.indexOf(':'));
+
 const router = express.Router();
 
+router.get(process.env.REACT_APP_BASE_DOT_API, async (req, res, next) => {
+  res.redirect(301, `${process.env.REACT_APP_HOST_PROTOCOL}${hostName}`);
+});
+
 router.get(process.env.REACT_APP_BASE_MISSING_API, async (req, res, next) => {
-  res.redirect(process.env.REACT_APP_BASE_ROUTE);
+  res.redirect(301, `${process.env.REACT_APP_HOST_PROTOCOL}${hostName}`);
 });
 
 export default router;
