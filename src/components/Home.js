8fa90dadc7145919cba36cf72aa71676ab723628
ASyedAkhtar <amersakhtar@gmail.com>
2022-06-04 15:13:06 -0400

    Updated Home component to reflect Post component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,8 @@
+import Post from './Post.js';
+
 const Home = () => {
   return(
-    <div>
-      <div id='newspictureone' class='news-picture popout box'></div>
-      <div className='news-content box'>
-        <h4>Website Grand Opening!</h4>
-        <h6>February 28th, 2021</h6>
-        <span>Hello world! Today marks the launch of the Suave International
-        website! After weeks of construction we are pleased to finally
-        open up to the public.
-        </span>
-      </div>
-    </div>
+    <Post />
   );
 }
 
