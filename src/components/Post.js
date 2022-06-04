d2d5e40f1a1d161c31b9f9d09fa0b3c3ee70d672
ASyedAkhtar <amersakhtar@gmail.com>
2022-06-04 15:12:41 -0400

    Added Post component

---
+++ src/components/Post.js
@@ -0,0 +1,18 @@
+const Post = () => {
+  return(
+    <div>
+      <div id='newspictureone' class='news-picture popout box'></div>
+      <div className='news-content box'>
+        <h4>Website Grand Opening!</h4>
+        <h6>February 28th, 2021</h6>
+        <span>Hello world! Today marks the launch of the Suave International
+          website! After weeks of construction we are pleased to finally
+          open up to the public.
+        </span>
+      </div>
+    </div>
+  );
+}
+  
+  export default Post;
+  
