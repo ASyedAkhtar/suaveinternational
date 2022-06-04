a4107aede10694b8566662ae7e9bf9d3302a0fe8
ASyedAkhtar <amersakhtar@gmail.com>
2022-06-03 20:35:37 -0400

    Changed log output format

--- src/server/middleware/logger.js
+++ src/server/middleware/logger.js
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs';
 
 const logger = (req, res, next) =>  {
-    console.log(`[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] Request made to ${req.protocol}://${req.get('host')}${req.originalUrl}:
-    \nRequest Secure: ${req.secure}\nRequest Headers: ${JSON.stringify(req.headers)}\n`);
+    console.log(`[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] Request made to ${req.protocol}://${req.get('host')}${req.originalUrl}
+    Request Secure: ${req.secure}\nRequest Headers: ${JSON.stringify(req.headers)}\n`);
     next();
 }
 
