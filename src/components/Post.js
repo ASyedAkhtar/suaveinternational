5663c801d22f20a44d327c5dc855e6d31593f904
ASyedAkhtar <amersakhtar@gmail.com>
2022-06-04 22:26:23 -0400

    Routed some fields from post API to component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,25 @@
-const Post = () => {
+const Post = ({post}) => {
   return(
-    <div>
-      <div id='newspictureone' class='news-picture popout box'></div>
-      <div className='news-content box'>
-        <h4>Website Grand Opening!</h4>
-        <h6>February 28th, 2021</h6>
-        <span>Hello world! Today marks the launch of the Suave International
-          website! After weeks of construction we are pleased to finally
-          open up to the public.
-        </span>
+    <div key={post._id} className='box'>
+      <div className='box'>
+        <h4>{post.title}</h4>
+        <h6>{post.date}</h6>
+        {post.paragraphs.map((postParagraph, i) => {
+          return(
+            <div>
+              <span>{postParagraph.text}</span>
+              {postParagraph.images.map((postParagraphImage, j) => {
+                return(
+                  <div className={`${post._id}_${i+1}_${j+1}`}></div>
+                );
+              })}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 }
   
-  export default Post;
+export default Post;
   
