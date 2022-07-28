138571962034f7a8c38578c2bb07128706cf8ba4
ASyedAkhtar <amersakhtar@gmail.com>
2022-07-28 15:11:58 -0400

    Removed box CSS class, removed post indices

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,26 +1,31 @@
 import blueglazedterracotta from './img/blueglazedterracotta.png';
+import basalttop from './img/basalttop.png';
 
 const Post = ({post}) => {
+
+  const images = {
+    blueglazedterracotta,
+    basalttop
+  };
+
   return(
-    <div key={post._id} className='box'>
-      <div className='box'>
-        <h4>{post.title}</h4>
-        <h6>{post.date}</h6>
-        {post.paragraphs.map((postParagraph, i) => {
-          return(
-            <div>
-              <span>{postParagraph.text}</span>
-              {postParagraph.images.map((postParagraphImage, j) => {
-                return(
-                  <img className={`${post._id}_${i+1}_${j+1}`} src={blueglazedterracotta} alt={postParagraphImage.caption}></img>
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
+    <div className='post'>
+      <h4>{post.title}</h4>
+      <h6>{post.date}</h6>
+      {post.paragraphs.map((postParagraph) => {
+        return(
+          <div>
+            {postParagraph.images.map((postParagraphImage) => {
+              return(
+                <img className='post-picture'/*{`${post._id}_${i+1}_${j+1}`} */ src={images[postParagraphImage.source]} alt={postParagraphImage.caption}></img>
+              );
+            })}
+            <span className='post-text'>{postParagraph.text}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
-  
+
 export default Post;
