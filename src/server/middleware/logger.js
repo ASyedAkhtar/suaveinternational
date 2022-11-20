6cd139bf826cb57d959dae5c63f809fb809f0544
ASyedAkhtar <amersakhtar@gmail.com>
2022-11-20 16:28:45 -0500

    Obtain and log client address

--- src/server/middleware/logger.js
+++ src/server/middleware/logger.js
@@ -1,9 +1,11 @@
 import dayjs from 'dayjs';
 
 const logger = (req, res, next) =>  {
-    console.log(`[${dayjs().format('YYYY-MM-DD HH:mm:ss')}] Request made to ${req.protocol}://${req.get('host')}${req.originalUrl}
-    Request Secure: ${req.secure}\nRequest Headers: ${JSON.stringify(req.headers)}\n`);
-    next();
+  console.log(`[${dayjs().format('YYYY-MM-DD HH:mm:ss')}]
+  Request Resource: ${req.protocol}://${req.get('host')}${req.originalUrl}
+  Request Origin: ${(req.header('x-forwarded-for') || req.socket.remoteAddress)}
+  Request Headers: ${JSON.stringify(req.headers)}\n`);
+  next();
 }
 
 export default logger;
